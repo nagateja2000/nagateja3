311ea4529cb61a09c1c99bec8257ab01a70dd80f
nagateja2000 <50978653+nagateja2000@users.noreply.github.com>
2019-05-24 11:59:53 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("enter two numbers\n");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("the sum of two numbers is %d",sum);
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
